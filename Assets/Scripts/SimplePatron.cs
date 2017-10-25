d8237b521861aa5e07cebffecc9c43d7666e298d
KRIPT4 <danielcsich@gmail.com>
2017-10-25 01:44:03 -0300

    Add Officer Level 1
    Add Officer Level 1

---
+++ Assets/Scripts/SimplePatron.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimplePatron : MonoBehaviour {
+    private Rigidbody2D playerOffice;
+    public GameObject startPoint;
+    public GameObject endPoint;
+    private Animator playerAnim;
+
+    public float enemySpeed;
+
+    private bool isGoingRight;
+
+	// Use this for initialization
+	void Start ()
+    {
+        playerOffice = GetComponent<Rigidbody2D>();
+        playerAnim = GetComponent<Animator>();
+        if (!isGoingRight)
+        {
+            transform.position = startPoint.transform.position;
+        }
+        else
+        {
+            transform.position = endPoint.transform.position;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (!isGoingRight)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, endPoint.transform.position,enemySpeed * Time.deltaTime);
+
+            if (transform.position == endPoint.transform.position)
+            {
+                isGoingRight = true;
+                GetComponent<SpriteRenderer>().flipX = true;
+            }
+
+        }
+        if (isGoingRight)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, startPoint.transform.position, enemySpeed * Time.deltaTime);
+            if (transform.position == startPoint.transform.position)
+            {
+                isGoingRight = false;
+                GetComponent<SpriteRenderer>().flipX = false;
+            }
+        }
+        playerAnim.SetFloat("vSpeed", Mathf.Abs(playerOffice.velocity.x));
+    }
+}

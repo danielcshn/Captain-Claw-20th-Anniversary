a86009b9b0dc84cdd84da5e31e4aff3ea995587f
KRIPT4 <danielcsich@gmail.com>
2017-11-02 18:00:43 -0300

    Rock Disintegrate Animation & Script
    Rock Disintegrate Animation & Script

---
+++ Assets/Scripts/RockDisintegrate.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockDisintegrate : MonoBehaviour {
+
+    private Animator anim;
+
+    public float fadeTime = 1f;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Awake()
+    {
+
+    }
+
+    IEnumerator OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+
+            anim.SetFloat("check", 1);
+
+            //AudioSource audio = GetComponent<AudioSource>();
+            //audio.Play();
+
+            yield return new WaitForSeconds(fadeTime);
+
+            Destroy(gameObject);
+        }
+    }
+}

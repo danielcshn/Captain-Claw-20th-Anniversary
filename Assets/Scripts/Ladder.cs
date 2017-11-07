eb5278f521db75fb003dce5352065f2a447ed3c7
KRIPT4 <danielcsich@gmail.com>
2017-11-07 15:12:09 -0300

    Claw Climbing Animation & Ladder Script Edit
    Claw Climbing Animation & Ladder Script Edit

--- Assets/Scripts/Ladder.cs
+++ Assets/Scripts/Ladder.cs
@@ -4,34 +4,86 @@ using UnityEngine;
 
 public class Ladder : MonoBehaviour {
 
-    GameObject playerOBJ;
-    public float speed = 6f;
+    public GameObject playerOBJ;
     private Rigidbody2D playerRB;
     private Animator playerAnim;
     bool canClimb = false;
+
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+    private bool grounded;
 
     void Start()
     {
         playerRB = GetComponent<Rigidbody2D>();
         playerAnim = GetComponent<Animator>();
+        
+    }
+
+    void FixedUpdate()
+    {
+        grounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
     }
 
     void OnTriggerStay2D(Collider2D other)
     {
+        other.GetComponent<Rigidbody2D>().gravityScale = 0f;
+        Animator animationObj = playerOBJ.GetComponentInChildren<Animator>();
         if (other.tag == "Player" && Input.GetAxis("Vertical") > 0)
         {
-            other.GetComponent<Rigidbody2D>().velocity = new Vector2(0, speed);
-            playerRB.gravityScale = 0f;
-            playerRB.mass = 0f;
+            other.GetComponent<Rigidbody2D>().transform.Translate(0, 1 * Time.deltaTime, 0);
+            //other.GetComponent<Rigidbody2D>().gravityScale = 0f;
+            animationObj.speed = 1;
+            animationObj.Play("claw_climbing");
+
         }
         else if (other.tag == "Player" && Input.GetAxis("Vertical") < 0)
         {
-            other.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -speed);
-            playerRB.mass = 0f;
-            playerRB.gravityScale = 0f;
+            other.GetComponent<Rigidbody2D>().transform.Translate(0, -1 * Time.deltaTime, 0);
+            //other.GetComponent<Rigidbody2D>().gravityScale = 0f;
+            animationObj.speed = 1;
+            animationObj.Play("claw_climbing");
+        }
+        else if (grounded)
+        {
+            animationObj.speed = 1;
+            animationObj.Play("claw_idle");
+            //animationObj.speed = 0; //pause animation
+            other.GetComponent<Rigidbody2D>().gravityScale = 2.8f;
+            playerOBJ.GetComponent<Rigidbody2D>().gravityScale = 2.8f;
         }
         else {
-            other.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 1);
+            animationObj.speed = 0;
         }
     }
+
+   /* private void OnTriggerExit(Collider other)
+    {
+        Animator animationObj = playerOBJ.GetComponentInChildren<Animator>();
+        animationObj.Play("claw_idle");
+        //animationObj.speed = 0; //pause animation
+        other.GetComponent<Rigidbody2D>().gravityScale = 2.8f;
+        playerOBJ.GetComponent<Rigidbody2D>().gravityScale = 2.8f;
+
+    }*/
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        Animator animationObj = playerOBJ.GetComponentInChildren<Animator>();
+        animationObj.speed = 1;
+        animationObj.Play("claw_idle");
+        //animationObj.speed = 0; //pause animation
+        other.GetComponent<Rigidbody2D>().gravityScale = 2.8f;
+        playerOBJ.GetComponent<Rigidbody2D>().gravityScale = 2.8f;
+        //other = null;
+    }
+
+    /*void OnTriggerEnter(Collider2D other)
+    {
+        if (!grounded)
+        {
+            other.GetComponent<Rigidbody2D>().gravityScale = 0f;
+        }
+    }*/
 }
